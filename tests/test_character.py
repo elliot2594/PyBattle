6f439713a164795768dfcd938c3be0d5d99a8b1d
elliot2594 <elliot.dodd94@gmail.com>
2024-02-12 21:27:49 +0000

    Added pytests

--- tests/test_character.py
+++ tests/test_character.py
@@ -6,15 +6,30 @@ from weapons import sword
 humanTestName = "Test_Human"
 orcTestName = "Test_Orc"
 
-class TestClass:
+class TestCharacterClass:
 
-	def test_HumanClass(self):
+	@pytest.fixture
+	def humanObject(self):
 		human_obj = Human(humanTestName)
-		assert human_obj.health == 100, "Should be 100"
-		assert human_obj.name == humanTestName, f"Name should be equal to {humanTestName}"
-		assert human_obj.equip(sword) == None
+		return human_obj
+
+	def test_HumanClass(self, humanObject):
+		assert humanObject.health == 100, "Should be 100"
+		assert humanObject.equip(sword) == None
+		
+
+	def testHealth(self, humanObject):
+		assert humanObject.health == 100, "Should be 100"
+
+	def testName(self, humanObject):
+		assert humanObject.name == humanTestName, f"Name should be equal to {humanTestName}"
+
+	def testSwordAttack(self, humanObject):
+		assert humanObject.equip(sword) == None
 		for i in range(0,100):
-			assert (1 <= int(human_obj.attack()) <= human_obj.weapon.damage), f"Damange should be between 1 and {human_obj.weapon.damage}"
+			assert (1 <= int(humanObject.attack()) <= humanObject.weapon.damage), f"Damange should be between 1 and {humanObject.weapon.damage}"
+
+
 
 	def testOrcClass(self):
 		orc_obj = Orc(orcTestName)
